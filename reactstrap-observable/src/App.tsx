4fc6b4aee2978267065a36e5af1c390cd90af688
franck ledoux <franck.ledoux.pro@gmail.com>
2018-08-04 20:49:48 +0200

    [reaactStrap] -  First commit

---
+++ reactstrap-observable/src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+import { Button } from 'reactstrap';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Button>HELLO</Button>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
