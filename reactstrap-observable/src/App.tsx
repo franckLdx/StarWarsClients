2233fa3540e02a1804c1955f395fa2e9e7c91d29
franck ledoux <franck.ledoux.pro@gmail.com>
2018-08-04 21:17:23 +0200

    update config

--- reactstrap-observable/src/App.tsx
+++ reactstrap-observable/src/App.tsx
@@ -1,9 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
 import './App.css';
-
 import logo from './logo.svg';
-
-import { Button } from 'reactstrap';
 
 class App extends React.Component {
   public render() {
@@ -13,7 +11,6 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Button>HELLO</Button>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
