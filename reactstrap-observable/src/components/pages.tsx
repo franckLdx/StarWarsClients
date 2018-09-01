712e62482c264d8982db221b1ebd688d9de55ab8
franck ledoux <franck.ledoux.pro@gmail.com>
2018-09-01 21:23:17 +0200

    Add pages component

---
+++ reactstrap-observable/src/components/pages.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
+interface IPageProps { pagesCount: number }
+
+export const Pages: React.StatelessComponent<IPageProps> = ({ pagesCount }) => {
+  if (pagesCount === 1) {
+    return null;
+  }
+  const numbers = new Array(pagesCount);
+  for (let index = 0; index < pagesCount; index++) {
+    numbers[index] = index;
+  }
+  return (
+    <Pagination>
+      <PaginationItem>
+        <PaginationLink previous={true} href="#1" />
+      </PaginationItem>
+      {
+        numbers.map(index => <PageIndex key={String(index + 1)} index={index + 1} />)
+      }
+      <PaginationItem>
+        <PaginationLink next={true} href={`#${pagesCount}`} />
+      </PaginationItem>
+    </Pagination >
+  )
+};
+
+const PageIndex: React.StatelessComponent<{ index: number }> = ({ index }) => (
+  <PaginationItem>
+    <PaginationLink href={`#${index}`}>
+      {index}
+    </PaginationLink>
+  </PaginationItem>
+)
