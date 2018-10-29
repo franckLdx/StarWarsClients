98ac0a31c64024d330738880a8cedbfc6ae12f5c
franck ledoux <franck.ledoux.pro@gmail.com>
2018-10-29 22:38:47 +0100

    Characters use observable.map

--- material-mobx/src/store/CharacterStore.ts
+++ material-mobx/src/store/CharacterStore.ts
@@ -1,11 +1,17 @@
-import { action, computed, observable, runInAction } from 'mobx';
+import {
+  action,
+  computed,
+  observable,
+  ObservableMap,
+} from 'mobx';
+
 import { IFetcher } from 'src/api';
 import { ICharacter } from 'src/model/Characters';
 
 export class CharaterStore {
-  @observable public characters: ICharacter[] = [];
+  @observable private characters: ObservableMap<string, ICharacter> = observable.map({});
   @observable private state: "NOT_LOADED" | "LOADING" | "LOADED" = "NOT_LOADED";
-  @observable private currentCharactersList: ICharacter[] = [];
+  @observable private currentCharactersList: string[] = [];
 
   constructor(private fetcher: IFetcher<ICharacter>) { }
 
@@ -14,40 +20,31 @@ export class CharaterStore {
     if (this.state !== 'NOT_LOADED') {
       return;
     }
-    const fetching: Array<Promise<ICharacter>> = [];
-    for (const id of ids) {
-      if (this.getById(id) === undefined) {
-        fetching.push(this.fetcher.fetchResource(id));
-      }
-    }
+    const fetching =
+      ids
+        .filter(id => !this.characters.has(id))
+        .map(id => this.fetcher.fetchResource(id));
     const chraracters = await Promise.all(fetching);
     this.addCharacters(...chraracters);
   }
 
   @action('set current characters list')
   public async setCurrentListById(...ids: string[]) {
-    const toDownload = ids.filter(id => this.getById(id) === undefined);
-    await this.fetchByIds(...toDownload);
-    runInAction('set current characters list rnInAction', () => {
-      this.currentCharactersList = ids.map(id => this.getById(id)!);
-    });
+    this.currentCharactersList = ids;
+    await this.fetchByIds(...ids);
   }
 
   @computed({ name: 'get current characters list' })
-  public get currentList(): ICharacter[] | null | undefined {
-    if (!this.currentCharactersList) {
-      return this.currentCharactersList;
-    }
-    return this.currentCharactersList
-      .slice().sort((c1, c2) => c1!.name < c2!.name ? -1 : 1);
+  public get currentList(): ICharacter[] {
+    const characters: any[] = this.currentCharactersList
+      .map(id => this.characters.get(id))
+      .filter(character => character !== undefined)
+    return characters.sort((c1, c2) => c1!.name < c2!.name ? -1 : 1);
   }
 
   @action('add characters')
   private addCharacters(...characters: ICharacter[]) {
-    this.characters = [...this.characters, ...characters];
+    characters.forEach(character => this.characters.set(character.id, character));
   }
 
-  private getById(id: string) {
-    return this.characters.find(character => character.id === id);
-  }
 }
